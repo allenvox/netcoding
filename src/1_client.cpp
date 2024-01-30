8867f71f255d9e1ab34423a569b4fe1d527a3410
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-01-30 23:46:58 +0700

    Format & comment code for lab 1

--- src/1_client.cpp
+++ src/1_client.cpp
@@ -1,65 +1,66 @@
-#include <iostream>
-#include <cstring>
 #include <arpa/inet.h>
-#include <unistd.h>
 #include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
 
 const int BUF_SIZE = 1024;
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <server_ip> <server_port>\n";
-        return 1;
-    }
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <server_ip> <server_port>\n";
+    return 1;
+  }
 
-    const char *server_ip = argv[1];
-    int server_port = atoi(argv[2]);
+  const char *server_ip = argv[1];
+  int server_port = atoi(argv[2]);
 
-    int sockfd;
-    struct sockaddr_in servaddr;
+  // Create a socket
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd == -1) {
+    std::cerr << "Failed to create socket\n";
+    return 1;
+  }
 
-    // Создание сокета
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
-        std::cerr << "Failed to create socket\n";
-        return 1;
-    }
-
-    // Заполнение структуры сервера
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(server_port);
-    if (inet_pton(AF_INET, server_ip, &servaddr.sin_addr) != 1) {
-        std::cerr << "Invalid address\n";
-        close(sockfd);
-        return 1;
-    }
-
-    for (int i = 1; i <= 5; ++i) { // Отправка 5 пакетов
-        char buffer[BUF_SIZE];
-        snprintf(buffer, BUF_SIZE, "Message %d from client", i);
-
-        // Отправка данных на сервер
-        if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-            std::cerr << "Error in sendto\n";
-            close(sockfd);
-            return 1;
-        }
-
-        // Получение ответа от сервера
-        memset(buffer, 0, BUF_SIZE);
-        socklen_t len = sizeof(servaddr);
-        int n = recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&servaddr, &len);
-        if (n == -1) {
-            std::cerr << "Error in recvfrom\n";
-            close(sockfd);
-            return 1;
-        }
-
-        // Отображение преобразованной информации от сервера
-        std::cout << "Received from server: " << buffer << std::endl;
-        sleep(i); // Задержка в i секунд
-    }
+  // Fill socket address structure for server
+  struct sockaddr_in servaddr;
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;          // IPv4
+  servaddr.sin_port = htons(server_port); // Set port to server_port
+  if (inet_pton(AF_INET, server_ip, &servaddr.sin_addr) != 1) {
+    std::cerr << "Invalid address\n";
     close(sockfd);
-    return 0;
+    return 1;
+  }
+
+  for (int i = 0; i < 5; ++i) { // 5 packages
+    // Create package message
+    char buffer[BUF_SIZE];
+    snprintf(buffer, BUF_SIZE, "Message %d from client", i);
+
+    // Send data to server
+    if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&servaddr,
+               sizeof(servaddr)) == -1) {
+      std::cerr << "Error in sendto\n";
+      close(sockfd);
+      return 1;
+    }
+
+    // Get response from server
+    memset(buffer, 0, BUF_SIZE);
+    socklen_t len = sizeof(servaddr);
+    int n = recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&servaddr,
+                     &len);
+    if (n == -1) {
+      std::cerr << "Error in recvfrom\n";
+      close(sockfd);
+      return 1;
+    }
+
+    // Print received info
+    std::cout << "Received from server: " << buffer << std::endl;
+    sleep(i); // Delay ('i' seconds)
+  }
+  close(sockfd);
+  return 0;
 }
--- src/1_server.cpp
+++ src/1_server.cpp
@@ -1,67 +1,66 @@
-#include <iostream>
-#include <cstring>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 const int BUF_SIZE = 1024;
 
 int main() {
-    int sockfd;
-    struct sockaddr_in servaddr, cliaddr;
+  // Create a socket
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd == -1) {
+    std::cerr << "Failed to create socket\n";
+    return 1;
+  }
 
-    // Создание сокета
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
-        std::cerr << "Failed to create socket\n";
-        return 1;
-    }
+  // Fill server structure
+  struct sockaddr_in servaddr;
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;                // Use IPv4
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen to any IP address
+  servaddr.sin_port = 0;                        // OS will find a free port
 
-    // Заполнение структуры сервера
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = 0; // Поиск свободного порта
-
-    // Привязка сокета к адресу и порту
-    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        std::cerr << "Bind failed\n";
-        close(sockfd);
-        return 1;
-    }
-
-    // Получение номера порта
-    socklen_t len = sizeof(servaddr);
-    getsockname(sockfd, (struct sockaddr *)&servaddr, &len);
-    std::cout << "Server started on port: " << ntohs(servaddr.sin_port) << std::endl;
-
-    char buffer[BUF_SIZE];
-    socklen_t clilen = sizeof(cliaddr);
-
-    while (true) {
-        // Получение данных от клиента
-        int n = recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&cliaddr, &clilen);
-        if (n == -1) {
-            std::cerr << "Error in recvfrom\n";
-            close(sockfd);
-            return 1;
-        }
-
-        // Отображение информации о клиенте
-        std::cout << "Received from " << inet_ntoa(cliaddr.sin_addr) << ':' << ntohs(cliaddr.sin_port) << " : " << buffer << std::endl;
-
-        // Отправка преобразованной информации обратно клиенту
-        if (sendto(sockfd, buffer, n, 0, (struct sockaddr *)&cliaddr, clilen) == -1) {
-            std::cerr << "Error in sendto\n";
-            close(sockfd);
-            return 1;
-        }
-    }
+  // Bind socket to address and port
+  if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    std::cerr << "Bind failed\n";
     close(sockfd);
-    return 0;
+    return 1;
+  }
+
+  // Get & print port number
+  socklen_t len = sizeof(servaddr);
+  getsockname(sockfd, (struct sockaddr *)&servaddr, &len);
+  std::cout << "Server started on port: " << ntohs(servaddr.sin_port)
+            << std::endl;
+
+  struct sockaddr_in cliaddr;
+  socklen_t clilen = sizeof(cliaddr);
+
+  char buffer[BUF_SIZE];
+  while (true) {
+    // Get data from any client
+    int n = recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *)&cliaddr,
+                     &clilen);
+    if (n == -1) {
+      std::cerr << "Error in recvfrom\n";
+      close(sockfd);
+      return 1;
+    }
+
+    // Print client info
+    std::cout << "Received from " << inet_ntoa(cliaddr.sin_addr) << ':'
+              << ntohs(cliaddr.sin_port) << " : " << buffer << std::endl;
+
+    // Send client's message back
+    if (sendto(sockfd, buffer, n, 0, (struct sockaddr *)&cliaddr, clilen) ==
+        -1) {
+      std::cerr << "Error in sendto\n";
+      close(sockfd);
+      return 1;
+    }
+  }
+  close(sockfd);
+  return 0;
 }
-
-
-
-
